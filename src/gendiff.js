91d60e01bfa87930efe5af0a38b20e28abf91ac9
Dmitry841 <strelx@mail.ru>
2020-11-09 14:40:00 +0300

    push eslint.yml

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,8 +2,8 @@ import program from 'commander';
 
 export default () => {
   program
-      .version('0.0.1')
-      .description('Compares two configuration files and shows a difference.');
-
+    .version('0.0.1')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]',  'output format');
     program.parse(process.argv);
   };
