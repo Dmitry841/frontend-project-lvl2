3703cb54ee3acd482705d5660f7fb1f2b8108a16
Dmitry841 <strelx@mail.ru>
2020-11-10 17:10:14 +0300

    make diff

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,9 +1,14 @@
 import program from 'commander';
+import genDiff from './index.js';
 
 export default () => {
   program
     .version('0.0.1')
     .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]',  'output format');
+    .option('-f, --format [type]',  'output format')
+    .command('compare <filepath1> <filepath2>')
+    .action(genDiff);
     program.parse(process.argv);
   };
+
+ 
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import getData from './dirtyfunctions.js';
+
+const genDiff = (filepath1, filepath2) => {
+  const [file1, file2] = getData(filepath1, filepath2);
+  const keys1 = Object.keys(file1).sort();
+  const keys2 = Object.keys(file2).sort();
+  const commonKeys = keys1.concat(keys2).sort();
+  const unicKeys = commonKeys.reduce((acc, key) => {
+    if (!acc.includes(key)) {
+      acc.push(key);
+    };
+    return acc;
+  }, []);
+  const difference = unicKeys.map((key) => {
+      if (!keys2.includes(key)) {
+        return `- ${key}: ${file1[key]}`;
+      } if (keys1.includes(key) && keys2.includes(key)) {
+          return file2[key] === file1[key] ? `  ${key}: ${file1[key]}`
+            : [`- ${key}: ${file1[key]}`, 
+               `+ ${key}: ${file2[key]}`];
+      } if (!keys1.includes(key)) {
+          return `+ ${key}: ${file2[key]}`;
+      }
+  })
+    .flat()
+      .map((item)=> `\n${item}`)
+        .join('');
+  console.log(`{${difference}\n}`);
+  return;
+};
+export default genDiff;
